85dcf277d90870c91ecc74df6166fc0cbb412e82
Jung Gyu Yoon <yjg30737@gmail.com>
2022-01-16 18:12:50 +0900

    Refactoring, fix some bugs, update version (0.0.1 -> 0.1.0)

--- pyqt_custom_frameless_mainwindow/customFramelessMainWindow.py
+++ pyqt_custom_frameless_mainwindow/customFramelessMainWindow.py
@@ -1,12 +1,12 @@
 from PyQt5.QtGui import QCursor
-from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QMainWindow, QApplication
 from PyQt5.QtCore import Qt, QPoint
 
 
 class CustomFramelessMainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.__margin = 3
+        self.__margin = 5
         self.__cursor = QCursor()
         self.__rect = 0
 
@@ -30,7 +30,7 @@ class CustomFramelessMainWindow(QMainWindow):
         self.setMouseTracking(True)
         self.setWindowFlags(Qt.FramelessWindowHint)
 
-    def setSizeCursor(self, p):
+    def __setSizeCursor(self, p):
         rect = self.rect()
         rect.setX(self.rect().x()+self.__margin)
         rect.setY(self.rect().y()+self.__margin)
@@ -46,8 +46,6 @@ class CustomFramelessMainWindow(QMainWindow):
             self.__bottom = False
             self.__right = False
             self.__left = False
-
-            self.__hoveredOnEdge = False
         else:
             # reshape cursor for resizing
             x = p.x()
@@ -58,87 +56,39 @@ class CustomFramelessMainWindow(QMainWindow):
             x2 = self.rect().width()
             y2 = self.rect().height()
 
-            # Top left
-            if abs(x-x1) <= self.__margin and abs(y-y1) <= self.__margin:
+            self.__left = abs(x-x1) <= self.__margin
+            self.__top = abs(y-y1) <= self.__margin
+            self.__right = abs(x-(x2+x1)) <= self.__margin
+            self.__bottom = abs(y-(y2+y1)) <= self.__margin
+
+            if self.__top and self.__left:
                 self.__cursor.setShape(Qt.SizeFDiagCursor)
-                self.__top = True
-                self.__left = True
-                self.__bottom = False
-                self.__right = False
-                self.setCursor(self.__cursor)
-
-            # Top right
-            elif abs(x-(x2+x1)) <= self.__margin and abs(y-y1) <= self.__margin:
+            elif self.__top and self.__right:
                 self.__cursor.setShape(Qt.SizeBDiagCursor)
-                self.__top = True
-                self.__right = True
-                self.__bottom = False
-                self.__left = False
-                self.setCursor(self.__cursor)
-
-            # Bottom left
-            elif abs(x-x1) <= self.__margin and abs(y-(y2+y1)) <= self.__margin:
+            elif self.__bottom and self.__left:
                 self.__cursor.setShape(Qt.SizeBDiagCursor)
-                self.__bottom = True
-                self.__left = True
-                self.__top = False
-                self.__right = False
-                self.setCursor(self.__cursor)
-
-            # Bottom right
-            elif abs(x-(x2+x1)) <= self.__margin and abs(y-(y2+y1)) <= self.__margin:
+            elif self.__bottom and self.__right:
                 self.__cursor.setShape(Qt.SizeFDiagCursor)
-                self.__bottom = True
-                self.__right = True
-                self.__top = False
-                self.__left = False
-                self.setCursor(self.__cursor)
-
-            # Top
-            elif abs(y-y1) <= self.__margin:
-                self.__top = True
-                self.__right = False
-                self.__left = False
-                self.__bottom = False
-                self.__cursor.setShape(Qt.SizeVerCursor)
-                self.setCursor(self.__cursor)
-
-            # Bottom
-            elif abs(y-(y2+y1)) <= self.__margin:
-                self.__bottom = True
-                self.__right = False
-                self.__left = False
-                self.__top = False
-                self.__cursor.setShape(Qt.SizeVerCursor)
-                self.setCursor(self.__cursor)
-
-            # Left
-            elif abs(x-x1) <= self.__margin:
-                self.__left = True
-                self.__right = False
-                self.__top = False
-                self.__bottom = False
+            elif self.__left:
                 self.__cursor.setShape(Qt.SizeHorCursor)
-                self.setCursor(self.__cursor)
-
-            # Right
-            elif abs(x-(x2+x1)) <= self.__margin:
-                self.__right = True
-                self.__left = False
-                self.__top = False
-                self.__bottom = False
+            elif self.__top:
+                self.__cursor.setShape(Qt.SizeVerCursor)
+            elif self.__right:
                 self.__cursor.setShape(Qt.SizeHorCursor)
-                self.setCursor(self.__cursor)
+            elif self.__bottom:
+                self.__cursor.setShape(Qt.SizeVerCursor)
+            self.setCursor(self.__cursor)
 
     def mousePressEvent(self, e):
-        if self.__hoveredOnEdge:
-            # resizing start
-            self.__resizeEnabled = True
-        else:
-            # moving start
-            p = e.pos()
-            self.__offset = p
-            self.__moving = True
+        if e.button() == Qt.LeftButton:
+            if self.__hoveredOnEdge:
+                # resizing start
+                self.__resizeEnabled = True
+            else:
+                # moving start
+                p = e.pos()
+                self.__offset = p
+                self.__moving = True
         return super().mousePressEvent(e)
 
     def mouseMoveEvent(self, e):
@@ -149,29 +99,29 @@ class CustomFramelessMainWindow(QMainWindow):
             x = p.x()
             y = p.y()
             if self.__cursor.shape() == Qt.SizeHorCursor:
-                if self.__left and self.__rect.right()-x > self.__min_width:
+                if self.__left:
                     self.__rect.setLeft(x)
-                elif self.__right and x > 30:
+                elif self.__right:
                     self.__rect.setRight(x)
             elif self.__cursor.shape() == Qt.SizeVerCursor:
-                if self.__top and self.__rect.bottom()-y > self.__min_height:
+                if self.__top:
                     self.__rect.setTop(y)
-                elif self.__bottom and y > self.__min_height:
+                elif self.__bottom:
                     self.__rect.setBottom(y)
             elif self.__cursor.shape() == Qt.SizeBDiagCursor:
-                if self.__top and self.__right and x > self.__min_width and self.__rect.bottom()-y > self.__min_height:
+                if self.__top and self.__right:
                     self.__rect.setTopRight(QPoint(x, y))
-                elif self.__bottom and self.__left and self.__rect.right()-x > self.__min_width and y > self.__min_height:
+                elif self.__bottom and self.__left:
                     self.__rect.setBottomLeft(QPoint(x, y))
             elif self.__cursor.shape() == Qt.SizeFDiagCursor:
-                if self.__top and self.__left and self.__rect.right()-x > self.__min_width and self.__rect.bottom()-y > self.__min_height:
+                if self.__top and self.__left:
                     self.__rect.setTopLeft(QPoint(x, y))
-                elif self.__bottom and self.__right and x > self.__min_width and y > self.__min_height:
+                elif self.__bottom and self.__right:
                     self.__rect.setBottomRight(QPoint(x, y))
         else:
             p = e.pos()
             # reshaping cursor
-            self.setSizeCursor(p)
+            self.__setSizeCursor(p)
             self.__hoveredOnEdge = not self.__isCursorHoveredOnInnerWidget(p)
             # if cursor shape indicates to resize
             if self.__hoveredOnEdge:
@@ -199,3 +149,12 @@ class CustomFramelessMainWindow(QMainWindow):
             # moving end
             self.__moving = False
         return super().mouseReleaseEvent(e)
+
+
+if __name__ == "__main__":
+    import sys
+
+    app = QApplication(sys.argv)
+    example = CustomFramelessMainWindow()
+    example.show()
+    app.exec_()
